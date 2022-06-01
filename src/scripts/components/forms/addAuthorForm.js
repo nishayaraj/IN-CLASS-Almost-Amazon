7750aef94cb6ac43d13caa2ccfb8b04db04a6409
nishaya raj <nishayaraj@gmail.com>
2022-05-31 21:36:07 -0500

    update authors working

--- src/scripts/components/forms/addAuthorForm.js
+++ src/scripts/components/forms/addAuthorForm.js
@@ -7,15 +7,15 @@ const addAuthorForm = (obj = {}) => {
     <form id="submit-author" class="mb-4">
       <div class="form-group">
         <label for="image">First Name</label>
-        <input type="text" class="form-control" id="first_name" placeholder="First Name" required>
+        <input type="text" class="form-control" id="first_name" placeholder="First Name" value="${obj.first_name || ''}" required>
       </div>
       <div class="form-group">
         <label for="image">Last Name</label>
-        <input type="text" class="form-control" id="last_name" placeholder="Last Name" required>
+        <input type="text" class="form-control" id="last_name" placeholder="Last Name" value="${obj.last_name || ''}" required>
       </div>
       <div class="form-group">
         <label for="title">Email</label>
-        <input type="email" class="form-control" id="email" aria-describedby="Email" placeholder="Enter Email" required>
+        <input type="email" class="form-control" id="email" aria-describedby="Email" placeholder="Enter Email" value="${obj.email || ''}" required>
       </div>
       <div class="form-check">
         <input type="checkbox" class="form-check-input" id="favorite" ${obj.favorite ? 'checked' : ''}>
